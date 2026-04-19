56006d54a1803cbc3feb40b3db7b498e65b74468
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2026-04-19 11:14:40 -0500

    fix(nginx): increase upload limit from 1MB to 50MB
    Nginx default client_max_body_size is 1MB, causing 413 errors on
    files >1MB before the request even reaches FastAPI. Add configurable
    MAX_UPLOAD_SIZE env var (default 50M) to nginx.conf.template and
    entrypoint.sh.

--- ui/entrypoint.sh
+++ ui/entrypoint.sh
@@ -2,7 +2,8 @@
 # Entrypoint script for frontend nginx container
 # Substitutes ${BACKEND_URL} at runtime so the same image works in any environment
 
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template \
+export MAX_UPLOAD_SIZE=${MAX_UPLOAD_SIZE:-50M}
+envsubst '${BACKEND_URL} ${MAX_UPLOAD_SIZE}' < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
